4aeaf8fdd625fccd0b031d5d495284f10ce5a700
Francisco Cano <volublecoffe@gmail.com>
2024-02-26 23:29:42 -0600

    commit

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_U3_constructor_y_destructor
+{
+    class MyObject
+    {
+        private int id;
+        public string Name { get; private set; }
+
+        public MyObject(int _id, string _name)
+        {
+            id = _id;
+            Name = _name;
+            Console.WriteLine($"Constructor of Object called for {Name}");
+        }
+
+        ~MyObject()
+        {
+            Console.WriteLine($"Destructor of Object called for {Name}");
+        }
+    }
+
+    class Container
+    {
+        private MyObject obj;
+
+        public Container(int id, string name)
+        {
+            obj = new MyObject(id, name);
+            Console.WriteLine($"Constructor of Container called for {obj.Name}");
+        }
+
+        ~Container()
+        {
+            Console.WriteLine($"Destructor of Container called for {obj.Name}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using POO_U3_constructor_y_destructor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyObject obj1 = new MyObject(1, "obj1");
+        Container container = new Container(2, "container");
+        MyObject obj2 = new MyObject(3, "obj2");
+    }
+}
